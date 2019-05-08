b119c801f83ac2f99c38582343b0825ff106a350
IgnacioSanhueza <ignacio.sanhueza@usach.cl>
2019-05-07 20:54:27 -0400

    Actualización

--- T1PP.py
+++ T1PP.py
@@ -1,75 +1,68 @@
 import match
+import visual
 import math
-import deepcopy from copy
+import copy
+
+def showPointList(l):
+    for i in range(len(l)):
+        print("("+str(l[i].x)+", "+str(l[i].y)+", "+str(l[i].color)+")")
 
 def aparear(n):
-	P = match.create_random_points(n)
-	xr = yr = 0
-	xa = ya=0
-	grpR=[]
-	grpA=[]
-	P=mergeList(P)
-	for i in range(len(P)):
-		if(P[i].color==-1):
-			grpR[i]=P.deepcopy(i)
-		else:
-			grpA[i]=P.deepcopy(i)
-	R=heuristic(P)
-	algorithm(grpR, grpA)
-	return 2 * len(R) / 2
+    P = match.create_random_points(n)
+    rectangulos=[]
+    showPointList(P)
+    aparear=[0]*len(P)
+    P.sort(key=lambda Point: Point.x)
+    print("Arreglo ordenado")
+    showPointList(P)
+    lCopy=copy.deepcopy(P)
+    print("Copia de arreglo ordenado")
+    showPointList(lCopy)
+    for i in range(int(math.sqrt(n))+1): #Se ejecutará (n)^1/2 veces el algoritmo
+        rectangulos+=algorithm(lCopy, lCopy, aparear)
+    for i in range(len(aparear)):
+        print(str(aparear[i]))
+    visual.Window(points=P,rectangles=rectangulos) 
+def restriccion(A, B, ListO, p, q):
+    for i in range(p, q):
+        if((A.x<ListO[i].x and A.y<ListO[i].x and B.x>ListO[i].x and B.y>ListO[i].y) or (A.x>ListO[i].x and A.y>ListO[i].x and B.x<ListO[i].x and B.y<ListO[i].y)):
+            return False
+    return True
+def binariSearch(k, l):
+    ini=0
+    top=len(l)-1
+    indice=0
+    flag = False
+    while ini<=top and not flag:
+        md=(ini+top)//2
+        if(l[md].x==k.x):
+            flag=True
+            indice=md
+        else:
+            if k.x<l[md].x:
+                top=md-1
+            else:
+                ini=md+1
+    return indice
 
-def heuristic(P):
-	showlistPoint(P)
-
-def algorithm(lPuntos, lRestriccion):
-	L=[] #Correspondiente a la lista de rectangulos obtenidos
-	dim=0
-	aux=0
-	yaux = 0
-	xaux = 0
-	for i in range(len(lPuntos)):
-		for j in range(i+1, len(lPuntos)):
-			if(dim<math.sqrt(((lPuntos[j].x-lPuntos[i].x)**2)+((lPuntos[j].y-lPuntos[i].y)**2))):
-				dim=math.sqrt(((lPuntos[j].x-lPuntos[i].x)**2)+((lPuntos[j].y-lPuntos[i].y)**2))
-				aux=j
-		if(L[0] == None):
-			yaux[] = [lPuntos[i].y,lPuntos[j].y]
-			xaux[] = [lPuntos[i].x,lPuntos[j].x]
-		else:
-			if lPuntos[i].x < lPuntos[j].x:
-
-# def dijkstra(D):		Estoy repensando esto del Dijkstra, quizas con solo ordenar sea suficiente
-# 	Para ello pienso que se puede crear una nueva lista de pares ordenados de pares ordenados Rec(P1,P2)
-# 	esto es porque me di cuenta de que cada punto no puede tener otro en el mismo eje x o y POR DEFINICION DEL PROBLEMA
-
-	return D
-
-def mergeList(A): #Avance 02/05, falta mota jaja
-	if len(A) >1:
-	md=len(arr)//2
-	l=A[:md]
-	r=A[mid:]
-	mergeList(l)
-	mergeList(r)
-	i=j=k=0
-	c=[]
-	while i<len(l) and j<len(r):
-		if(l[i].x<=r[j].x):
-			c.append(A[i])
-			i+=1
-		else:
-			c.append(B[j])
-			j+=1
-	while i<len(A):
-		c.append(A[i])
-		i+=1
-	while j<len(B):
-		c.append(B[i])
-		j+=1
-	return c
-
-def showlistPoint(P):
-	for i in range(P):
-		print("("+str(i.x)+", "+str(i.y)+", "+str(i.color)+")")
-
-aparear(5)
+def algorithm(lPuntos, lRestriccion, aparear):
+    dim=10**1000000
+    auxj=0
+    r=0
+    rectangulos=[]
+    for i in range(len(lPuntos)):
+        if(aparear[i]==0):
+            for j in range(i+1, len(lPuntos)):
+                if(aparear[j]==0):
+                    r=binariSearch(lPuntos[i], lRestriccion)
+                    if(dim>math.sqrt(((lPuntos[j].x-lPuntos[i].x)**2)+((lPuntos[j].y-lPuntos[i].y)**2)) and restriccion(lPuntos[i], lPuntos[j], lRestriccion, r, j) and lPuntos[i].color==lPuntos[j].color):
+                        dim=math.sqrt(((lPuntos[j].x-lPuntos[i].x)**2)+((lPuntos[j].y-lPuntos[i].y)**2))
+                        auxj=j
+            if(auxj!=0):
+                rectangulos.append(match.Rectangle(min(lPuntos[i].x,lPuntos[auxj].x), max(lPuntos[i].x,lPuntos[auxj].x), min(lPuntos[i].y,lPuntos[auxj].y), max(lPuntos[i].y,lPuntos[auxj].y)))
+                aparear[i]=1
+                aparear[auxj]=1
+                auxj=0
+            r=0
+    return rectangulos
+aparear(1000)
