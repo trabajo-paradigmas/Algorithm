7482596455626bc759584bd93d0001ad246c51de
IgnacioSanhueza <ignacio.sanhueza@usach.cl>
2019-05-23 23:32:08 -0400

    Agregar archivos Trabajo 2 - Scheme

--- Python/visual.py
+++ Python/visual.py
@@ -158,6 +158,6 @@ class Window:
             
     def take_picture(self,filename=None):
         if filename == None:
-            filename=asksaveasfilename(filetypes=[("postscript","*.ps *.eps")],defaultextension=".ps")
+            filename=filename(filetypes=[("postscript","*.ps *.eps")],defaultextension=".ps")
         self.paper.postscript(file=filename)
         
