dea5badda727492f924074de403a1635e0cdb57a
IgnacioSanhueza <ignacio.sanhueza@usach.cl>
2019-05-25 20:11:03 -0400

    comenzando... función merge

--- Scheme/Funciones merge sort e insertion sort.rkt
+++ Scheme/Funciones merge sort e insertion sort.rkt
@@ -1,3 +1,8 @@
 (define (merge A B)
-  
+  (cond
+    ((null? A) B)
+    ((null? B) A)
+    ((< (car A) (car B)) (cons (car A) (merge (cdr A) B)))
+    (else (cons (car B) (merge A (cdr B))))
+    )
   )
