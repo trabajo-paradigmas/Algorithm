9547cfdf3b4ff59ec567ab524a7f10fd819167cd
IgnacioSanhueza <ignacio.sanhueza@usach.cl>
2019-06-16 17:20:15 -0400

    Backtraking algorithm

--- Python/T1PP.py
+++ Python/T1PP.py
@@ -1,5 +1,3 @@
-import copy
-import math
 import time
 
 import match
@@ -16,8 +14,6 @@ def aparear(n):
     Px = match.create_random_points(n)
     rectangulos = []
     Px.sort(key=lambda Point: Point.x)
-    Py = copy.deepcopy(Px)
-    Py.sort(key=lambda Point: Point.y)
     rectangulos += algorithm(Px, Py)
     porcentaje = (len(rectangulos) * 2 * 100) / len(Px)
     ejecucion = time.time() - tiempo
@@ -29,139 +25,35 @@ def aparear(n):
 
 
 def not_in_R(Rec, P1, P2):
-    if len(Rec) == 0:
-        return True
-    for i in range(len(Rec)):
-        if ((Rec[i].left < min(P1.x, P2.x) and Rec[i].right > max(P1.x, P2.x)) and (
-                Rec[i].bottom < min(P1.y, P2.y) and Rec[i].top > max(P1.y, P2.y))):
-            return False
+    for i in range(len(Rec, -1):
+        if
+    (Rec[i].right < P1.x):
     return True
-
-
-def restriccion(A, B, ListO, p, q):
-    for i in range(p, q):
-        x = int(ListO[i].x)
-        y = int(ListO[i].y)
-        if (A.std == True or B.std == True):
-            return False
-        if (A.color != B.color):
-            return False
-        if ((x > min(A.x, B.x) and x < max(A.x, B.x)) or (y > min(A.y, B.y) and y < max(A.y, B.y))):
-            return False
+    elif (((P1.x > Rec[i].right) and (P2.x > Rec[i].left)) or ((P1.y < Rec[i].bottom) and (P2.y < Rec[i].bottom)) or (
+                (P1.y > Rec[i].bottom) and (P2.y > Rec[i].bottom)))):
     return True
+    elif (((P1.x > Rec[i].right) and (P1.y < Rec[i].bottom)) and ((P2.x > Rec[i].right) and (P1.y < Rec[i].bottom))):
 
 
-def distanceFuntion(P1, P2):
-    return math.sqrt(((P2.x - P1.x) ** 2) + ((P2.y - P1.y) ** 2))
-
-
-def binariSearch(k, l):
-    ini = 0
-    top = len(l) - 1
-    indice = -1
-    flag = False
-    while ini <= top and not flag:
-        md = (ini + top) // 2
-        if l[md].x == k.x:
-            flag = True
-            indice = md
-        else:
-            if k.x < l[md].x:
-                top = md - 1
-            else:
-                ini = md + 1
-    return indice
-
-
-def next_point(indice, lista, op=1):
-    k = indice + op
-
-    while (lista[indice].color != lista[k].color) and (k >= len(lista) or k < 0):
-        k = k + op
-    return k
-
-
-def algorithm(XPuntos, YPuntos):
+def algorithm(Puntos):
     rectangulos = []
-    xs = 0
-    xr = 0
-    ys = 0
-    yr = 0
-    dim = 10 ** 1000000
-    for i in range(len(YPuntos)):
-        if i == 0:
-            x = binariSearch(YPuntos[i], XPuntos)
-            if x == 0:
-                xs = next_point(x, XPuntos)
-                ys = next_point(i, YPuntos)
-            elif x == (len(XPuntos) - 1):
-                xr = next_point(x, XPuntos, -1)
-                ys = next_point(i, YPuntos)
-            else:
-                xs = next_point(x, XPuntos)
-                xr = next_point(x, XPuntos, -1)
-                ys = next_point(i, YPuntos)
-        elif i == (len(YPuntos) - 1):
-            x = binariSearch(YPuntos[i], XPuntos)
-            if x == 0:
-                xs = next_point(x, XPuntos)
-                yr = next_point(i, YPuntos, -1)
-            elif x == (len(XPuntos) - 1):
-                xr = next_point(x, XPuntos, -1)
-                yr = next_point(i, YPuntos, -1)
-            else:
-                xs = next_point(x, XPuntos)
-                xr = next_point(x, XPuntos, -1)
-                yr = next_point(i, YPuntos, -1)
-        else:
-            x = binariSearch(YPuntos[i], XPuntos)
-            if x == 0:
-                xs = next_point(x, XPuntos)
-                ys = next_point(i, YPuntos)
-                yr = next_point(i, YPuntos, -1)
-            elif x == (len(XPuntos) - 1):
-                xr = next_point(x, XPuntos, -1)
-                yr = next_point(i, YPuntos, -1)
-                ys = next_point(i, YPuntos)
-            else:
-                xs = next_point(x, XPuntos)
-                xr = next_point(x, XPuntos, -1)
-                yr = next_point(i, YPuntos, -1)
-                ys = next_point(i, YPuntos)
-        punto = [0, 0]
-        aux = [xs, xr, ys, yr]
-        aux1 = 10 ** 1000000
-        for e in aux:
-            if e == xs or e == xr:
-                aux1 = distanceFuntion(YPuntos[i], XPuntos[e])
-                if ((dim > aux1) and ((restriccion(YPuntos[i], XPuntos[e], YPuntos, i - 1, i + 1) != False) and (
-                        restriccion(YPuntos[i], XPuntos[e], XPuntos, x - 1, x + 1) != False))):
-                    dim = aux1
-                    punto = [e, 1]
-            elif e == ys or e == yr:
-                aux1 = distanceFuntion(YPuntos[i], YPuntos[e])
-                if ((dim > aux1) and ((restriccion(YPuntos[i], YPuntos[e], YPuntos, i - 1, i + 1) == True) and (
-                        restriccion(YPuntos[i], YPuntos[e], YPuntos, x - 1, x + 1) == True))):
-                    dim = aux1
-                    punto = [e, -1]
-        p1 = punto[0]
-        p2 = punto[1]
-        if (p2 == 1) and (not_in_R(rectangulos, YPuntos[i], YPuntos[p1]) == True):
-            r = match.Rectangle(min(YPuntos[x].x, YPuntos[p1].x), max(YPuntos[x].x, YPuntos[p1].x),
-                                min(YPuntos[x].y, YPuntos[p1].y),
-                                max(YPuntos[x].y, YPuntos[p1].y))
-            YPuntos[i].std = True
-            YPuntos[p1].std = True
-            print("Agregando...")
-            rectangulos.append(r)
-        elif (not_in_R(rectangulos, YPuntos[i], XPuntos[p1]) == True):
-            r = match.Rectangle(min(YPuntos[x].x, XPuntos[p1].x), max(YPuntos[x].x, XPuntos[p1].x),
-                                min(YPuntos[x].y, XPuntos[p1].y),
-                                max(YPuntos[x].y, XPuntos[p1].y))
-            YPuntos[i].std = True
-            XPuntos[p1].std = True
-            print("Agregando...")
-            rectangulos.append(r)
+    for i in range(len(Puntos) - 1):
+        rest_y_arr = -1
+        rest_x = -1
+        rest_y_abj = -1
+        if ((i + 1) < len(Puntos)):
+            j = i + 1
+            while ((abs(rest_y_arr - rest_y_abj)) != 1):
+                if Puntos[i].color == Puntos[j].color:
+                    if not_in_R(rectangulos, Puntos[i], Puntos[j]):
+                        r = match.Rectangle(min(Puntos[i].x, Puntos[j].x), max(Puntos[i].x, Puntos[j].x),
+                                            min(Puntos[i].y, Puntos[j].y), max(Puntos[i].y, Puntos[j].y))
+                        rectangulos.append(r)
+                        break
+                elif (Puntos[j].y < Puntos[i].y or (rest_y_abj < Puntos[j].y)):
+                    rest_y_abj = Puntos[j].y
+                elif (Puntos[j].y > Puntos[i].y or (rest_y_arr > Puntos[j].y)):
+                    rest_y_arr = Puntos[j].y
     return rectangulos
 
 
--- Scheme/Trabajo 2 - Timsort y Generador Fibonacci (Scheme).rkt
+++ Scheme/Trabajo 2 - Timsort y Generador Fibonacci (Scheme).rkt
@@ -27,7 +27,7 @@
     )
   )
 
-; Devuelve la mitad menor del largo de una lista
+; Devuelve el largo de la mitad menor de una lista
 (define (mid_length L)
   (quotient (length L) 2)
   )
@@ -47,45 +47,96 @@
   (split_at L (mid_length L))
   )
 
-;Ordenamiento por inserción por medio de una operación binaria
-(define (in_sort B op)
+;Ordenamiento por mezcla
+(define (merge_sort L op)
+  (if; Si
+   (<= (length L) 1); El largo es igual o menor a 1
+   L; Se devuelve la lista
+   (let*;De lo contrario, sea:
+       (
+        (AB (split L)); AB igual a la partición a la mitad de la lista
+        (A (merge_sort (car AB) op)); A igual a seguir ordenando por la primera mitad
+        (B (merge_sort (car (cdr AB)) op)); B igual a seguir ordenando por la segunda mitad
+        )
+     (merge A B op); Se retorna el ordenamiento por mezcla de ambas mitades
+     )
+   )
+  )
+
+;Ordenamiento por inserción
+(define (in_sort B)
   (if
    (or (null? B) (null? (cdr B))); Si es vacía se devuelve la misma lista
    B
-   (insert (car B) (in_sort (cdr B) op) op); De lo contrario se toma el primer elemento para ser insertado en lo que retorna el ordenar el resto de la lista
+    (insert (car B) (in_sort (cdr B))); De lo contrario se toma el primer elemento para ser insertado en lo que retorna el ordenar el resto de la lista
    )
   )
 
 ; Inserta en orden un elemento a lista
-(define (insert x B op)
+(define (insert x B)
+  (define (cmp_keys x y) (< (car x) (car y))); Define una funcíon de comparación
+  (if; Si
+   (or (null? B) (cmp_keys x (car B)));  O la lista esta vacía, o la comparación del elemento da ser menor al primer elemento de la lista
+   (cons x B); Se concatena el elemento a la lista
+   (cons (car B) (insert x (cdr B))); De lo contrario se concatena el primer elemento de la listo con lo que resta por insertar en la lista
+   )
+  )
+
+;Ordenamiento por insersión
+(define (insertion_sort A key)
+  (define (key_elem x) (list (key x) x))
   (if
-   (or (null? B) (op x (car B))); Si es vacío o la comparación es verdadera
-   (cons x B); Se concatena el elemento a insertar con el resto de la lista
-   (cons (car B) (insert x (cdr B) op)); De lo contrario se concatena el primer elemento con el resultado de ordenar la lista restante
+   (or (null? A) (null? (cdr A)))
+   A
+   (let*
+       (
+        (B (map key_elem A))
+        (C (in_sort B))
+        )
+     (map cadr C)
+     )
    )
   )
   
 ;Timsort
 (define (timsort L op)
-  (define (Tim_aux L largo op);Usa el algoritmo de mergesort
-    (if
-     (= (length L) 1); Si tiene un elemento
-     L; Se devuelve la lista ya que un elemento único está ordenado
-     (if
-      (< largo 18); Si se encuentra en el punto óptimo de galope (18 para Scheme)
-      (in_sort L op); Entonces devuelve lista ordenada por inserción
-      (let*; De lo contrario
-          (;Sea
-           (AB (split L)); AB la separación en mitades de la lista
-           (A (Tim_aux (car AB) (length (car AB)) op)); Se ordena una mitad por mergesort
-           (B (Tim_aux (car (cdr AB)) (length (cdr AB)) op)); Se ordena la otra mitad por mergesort
-           )
-        (merge A B op); Se unen ambas listas con merge
-        )
+  
+  (define (cmp op x y)
+    (cond
+      (< (op x) (op y)) (car (list 1))
+      (> (op x) (op y)) (car (list -1))
+      (= (op x) (op y)) (car (list 0))
       )
-     )
     )
-  (Tim_aux L (length L) op)
+  
+  (define (OP_merge L1 L2 op)
+    (merge_sort L1 L2 (lambda(x y)(< (op x)(op y))))
+    )
+  
+  (define (rev L)
+    (define (rev2 A B)
+      (if
+       (null? B)
+       A
+       (rev2
+        (cons (car B) A)
+        (cdr B)
+        )
+       )
+      )
+    (rev2 (list) L)
+    )
+  
+  (define (subsec op_ant L acc); Cuenta en dónde realizar el corte mediante (split_at) y retorna una lista de subsequencias ordenadas
+    (let*
+        (
+         ()
+         )
+      ()
+        )
+    )
+    
+  (subsec 2 L 1)
   )
 ;El punto óptimo del galope fue calculado mididendo diferentes tiempos de ejecución para diferentes largos de listas, que se incrementan en uno por cada prueba nueva,
 ;hasta que el algoritmo por insersión resultara por primera vez pase de tener menor tiempor de ejecución frente al ordenamiento por mezcla
